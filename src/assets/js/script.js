774bf3a3eba0e0de894aec0f83d28c0ba379eeee
Emmanuel <emmanuel.cohen00@gmail.com>
2022-06-22 23:12:35 +0200

    add second collapse and transition

--- src/assets/js/script.js
+++ src/assets/js/script.js
@@ -1,16 +1,17 @@
 var coll = document.getElementsByClassName("collapsible");
-var croix = document.getElementById("closeListCollapse");
+
 
 for (var i = 0; i < coll.length; i++) {
   coll[i].addEventListener("click", function() {
     this.classList.toggle("active");
     var content = this.nextElementSibling;
+    var croix = this.firstElementChild;
     if (content.style.display === "block") {
       content.style.display = "none";
-      croix.setAttribute("style", "transform: rotate(45deg);");
+      croix.setAttribute("style", "transform: rotate(45deg); transition: transform 200ms ease-out;");
     } else {
       content.style.display = "block";
-      croix.setAttribute("style", "transform: rotate(0deg);");
+      croix.setAttribute("style", "transform: rotate(0deg);  transition: transform 200ms ease-out;");
     }
   });
 }
