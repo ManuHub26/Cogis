bd6e4ab327442bd5c5a11e92e8ed422a030533ea
Emmanuel <emmanuel.cohen00@gmail.com>
2022-06-30 09:36:35 +0200

    Add opacity on collapsable for better transition

--- src/assets/js/script.js
+++ src/assets/js/script.js
@@ -2,15 +2,15 @@ var coll = document.getElementsByClassName("collapsible");
 
 
 for (var i = 0; i < coll.length; i++) {
-  coll[i].addEventListener("click", function() {
+  coll[i].addEventListener("click", function () {
     this.classList.toggle("active");
     var content = this.nextElementSibling;
     var croix = this.firstElementChild;
-    if (content.style.display === "block") {
-      content.style.display = "none";
+    if (content.style.opacity === "1") {
+      content.style.opacity = "0";
       croix.setAttribute("style", "transform: rotate(45deg); transition: transform 200ms ease-out;");
     } else {
-      content.style.display = "block";
+      content.style.opacity = "1";
       croix.setAttribute("style", "transform: rotate(0deg);  transition: transform 200ms ease-out;");
     }
   });
